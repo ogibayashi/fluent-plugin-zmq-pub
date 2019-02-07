f6531624a2c0f77c7a4817c5258315518dd09a45
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2019-02-07 19:20:24 +0900

    out_zmq_pub: Migrate to use Fluentd v1 API

--- test/plugin/test_out_zeromq_pub.rb
+++ test/plugin/test_out_zeromq_pub.rb
@@ -1,5 +1,6 @@
 require 'helper'
 require 'ffi-rzmq'
+require 'fluent/test/driver/output'
 
 class ZmqPubOutputTest < Test::Unit::TestCase
   def setup
@@ -13,7 +14,7 @@ class ZmqPubOutputTest < Test::Unit::TestCase
     @subscriber.close
     @context.terminate
   end
-  
+
   CONFIG = %[
       pubkey ${tag}:${key1}
       bindaddr tcp://*:5556
@@ -29,45 +30,89 @@ class ZmqPubOutputTest < Test::Unit::TestCase
       pubkey ${tag}
       bindaddr tcp://*:5556
   ]
-  
-  def create_driver(conf = CONFIG, tag='test')
-    Fluent::Test::BufferedOutputTestDriver.new(Fluent::ZmqPubOutput, tag).configure(conf)
+
+  def create_driver(conf = CONFIG)
+    Fluent::Test::Driver::Output.new(Fluent::Plugin::ZmqPubOutput).configure(conf)
   end
-  
+
   def test_configure
     d = create_driver
 
     assert_equal '${tag}:${key1}', d.instance.pubkey
     assert_equal 'tcp://*:5556', d.instance.bindaddr
   end
-  
+
   def test_format
     d = create_driver
-    
+
     time = Time.parse("2011-01-02 13:14:15 UTC").to_i
-    d.emit({"key1"=>"aaa"}, time)
-    d.emit({"key1"=>"bbb", "key2"=>3}, time)
-    
-    d.expect_format ["test",time.to_i,{ "key1" => "aaa"}].to_msgpack
-    d.expect_format ["test",time.to_i,{ "key1" => "bbb", "key2" => 3}].to_msgpack
-    
-    d.run
+    d.run(default_tag: 'test') do
+      d.feed(time, {"key1"=>"aaa"})
+      d.feed(time, {"key1"=>"bbb", "key2"=>3})
+    end
+
+    assert_equal ["test",time.to_i,{ "key1" => "aaa"}].to_msgpack, d.formatted[0]
+    assert_equal ["test",time.to_i,{ "key1" => "bbb", "key2" => 3}].to_msgpack, d.formatted[1]
   end
-  
+
   def test_write
-    d = create_driver
+    config = Fluent::Config::Element.new('ROOT', '', {
+                                           '@type' => 'zmq_pub',
+                                           'pubkey' => '${tag}:${key1}',
+                                           'bindaddr' => 'tcp://*:5556'
+                                         }, [
+                                           Fluent::Config::Element.new('buffer', 'tag,key1', {
+                                                                         '@type' => 'memory',
+                                                                       },[] )
+                                         ])
+    d = create_driver(config)
     @subscriber.setsockopt(ZMQ::SUBSCRIBE,"test:aaa")
-    
+
     time = Time.parse("2011-01-02 13:14:15 UTC").to_i
-    d.emit({"key1"=>"aaa"}, time)
-    d.emit({"key1"=>"bbb", "key2"=>3}, time)
-    d.emit({"key1"=>"aaa", "key2"=>4}, time)
-    
-    d.run
+    msg = ''
+    d.run(default_tag: 'test', shutdown: false) do
+      d.feed(time, {"key1"=>"aaa"})
+      d.feed(time, {"key1"=>"bbb", "key2"=>3})
+      d.feed(time, {"key1"=>"aaa", "key2"=>4})
+    end
+
     sleep 1
+    @subscriber.recv_string(msg,ZMQ::DONTWAIT)
+    (key, record) = msg.split(" ",2)
+    assert_equal ["test",time.to_i,{ "key1" => "aaa"}].to_msgpack, record
 
     msg = ''
     @subscriber.recv_string(msg,ZMQ::DONTWAIT)
+    d.instance.shutdown # dispose
+    (key, record) = msg.split(" ",2)
+    assert_equal ["test",time.to_i,{ "key1" => "aaa","key2" => 4 }].to_msgpack, record
+
+  end
+
+  def test_write_bulk
+    config_bulk = Fluent::Config::Element.new('ROOT', '', {
+                                                '@type' => 'zmq_pub',
+                                                'pubkey' => '${tag}:${key1}',
+                                                'bindaddr' => 'tcp://*:5556'
+                                              }, [
+                                                Fluent::Config::Element.new('buffer', 'tag,key1', {
+                                                                              '@type' => 'memory',
+                                                                            },[] )
+                                              ])
+    d = create_driver(config_bulk)
+    @subscriber.setsockopt(ZMQ::SUBSCRIBE,"test:aaa")
+
+    time = Time.parse("2011-01-02 13:14:15 UTC").to_i
+    msg = ''
+    d.run(default_tag: 'test', shutdown: false) do
+      d.feed(time, {"key1"=>"aaa"})
+      d.feed(time, {"key1"=>"bbb", "key2"=>3})
+      d.feed(time, {"key1"=>"aaa", "key2"=>4})
+    end
+
+    sleep 1
+    @subscriber.recv_string(msg,ZMQ::DONTWAIT)
+    d.instance.shutdown # dispose
     (key, record) = msg.split(" ",2)
     assert_equal ["test",time.to_i,{ "key1" => "aaa"}].to_msgpack, record
 
@@ -78,27 +123,5 @@ class ZmqPubOutputTest < Test::Unit::TestCase
 
   end
 
-  def test_write_bulk
-    d = create_driver(CONFIG_BULK)
-    @subscriber.setsockopt(ZMQ::SUBSCRIBE,"test:aaa")
-    
-    time = Time.parse("2011-01-02 13:14:15 UTC").to_i
-    d.emit({"key1"=>"aaa"}, time)
-    d.emit({"key1"=>"bbb", "key2"=>3}, time)
-    d.emit({"key1"=>"aaa", "key2"=>4}, time)
-    
-    d.run
-    sleep 1
-
-    msg = ''
-    @subscriber.recv_string(msg,ZMQ::DONTWAIT)
-    (key, record) = msg.split(" ",2)
-    assert_equal [["test",time.to_i,{ "key1" => "aaa"}],
-                  ["test",time.to_i,{ "key1" => "aaa","key2" => 4 }]].to_msgpack, record
-
-  end
-
 
 end
-
-
