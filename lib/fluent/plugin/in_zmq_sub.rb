909abbb349333ec7f9126424c7769fb831f95188
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2019-02-08 14:15:08 +0900

    in_zmq_sub: Migrate to use Fluentd v1 API

--- lib/fluent/plugin/in_zmq_sub.rb
+++ lib/fluent/plugin/in_zmq_sub.rb
@@ -1,11 +1,13 @@
-require 'fluent/input'
+require 'fluent/plugin/input'
 
-module Fluent
+module Fluent::Plugin
 
-  class ZmqSubInput < Fluent::Input
+  class ZmqSubInput < Input
     Fluent::Plugin.register_input('zmq_sub', self)
 
-    config_param :subkey, :string, :default => ""
+    helpers :thread
+
+    config_param :subkey, :array, :default => []
     config_param :publisher, :string, :default => "tcp://127.0.0.1:5556"
     config_param :bulk_send, :bool, :default => false
 
@@ -18,69 +20,64 @@ module Fluent
 
     def configure(conf)
       super
-      @subkeys = @subkey.split(",")
+      @subkeys = @subkey # for compatibility.
+      @unpacker = Fluent::Engine.msgpack_factory.unpacker
     end
 
     def start
       super
       @context =ZMQ::Context.new()
-      @thread = Thread.new(&method(:run))
+      thread_create(:in_zmq_sub_runner, &method(:run))
     end
 
     def shutdown
-      Thread.kill(@thread)
-      @thread.join
+      if @subscriber
+        @subscriber.close
+      end
+
       @context.terminate
       super
     end
 
     def run
-      begin
-        @subscriber = @context.socket(ZMQ::SUB)
-        @subscriber.connect(@publisher)
-        if @subkeys.size > 0
-          @subkeys.each do |k|
-            @subscriber.setsockopt(ZMQ::SUBSCRIBE,k)
-          end
-        else
-          @subscriber.setsockopt(ZMQ::SUBSCRIBE,'')
+      @subscriber = @context.socket(ZMQ::SUB)
+      @subscriber.connect(@publisher)
+      if @subkeys.size > 0
+        @subkeys.each do |k|
+          @subscriber.setsockopt(ZMQ::SUBSCRIBE,k)
         end
-        loop do
-          msg = ''
-          while @subscriber.recv_string(msg,ZMQ::DONTWAIT) && msg.size > 0
-            begin
-              (key, records) = msg.split(" ",2)
-              records = MessagePack.unpack(records)
-              if @bulk_send && records[0].class == Array
-                es = MultiEventStream.new
+      else
+        @subscriber.setsockopt(ZMQ::SUBSCRIBE,'')
+      end
+      loop do
+        msg = ''
+        while @subscriber.recv_string(msg,ZMQ::DONTWAIT) && msg.size > 0
+          begin
+            (key, records) = msg.split(" ",2)
+            @unpacker.feed_each(records) do |obj|
+              if @bulk_send && obj[0].class == Array
+                es = Fluent::MultiEventStream.new
                 prev_tag = nil
-                records.each do |tag, time, record|
+                obj.each do |tag, time, record|
                   if prev_tag && prev_tag != tag
-                    Engine.emit_stream(prev_tag, es)
-                    es = MultiEventStream.new
+                    router.emit_stream(prev_tag, es)
+                    es = Fluent::MultiEventStream.new
                   end
                   es.add(time, record)
                   prev_tag = tag
                 end
-                Engine.emit_stream(prev_tag, es) if es.to_a.size > 0
+                router.emit_stream(prev_tag, es) if es.to_a.size > 0
               else
-                Engine.emit(*records)
+                router.emit(*obj)
               end
-            rescue => e
-              log.warn "Error in processing message.",:error_class => e.class, :error => e
-              log.warn_backtrace
             end
-            msg = ''
+          rescue => e
+            log.warn "Error in processing message.",:error_class => e.class, :error => e
+            log.warn_backtrace
           end
-          sleep(0.1)
+          msg = ''
         end
-      rescue => e
-        log.error "error occurred while executing plugin.", :error_class => e.class, :error => e
-        log.warn_backtrace
-      ensure
-        if @subscriber
-          @subscriber.close
-        end
+        sleep(0.1)
       end
     end
 
--- test/plugin/test_in_zmq_sub.rb
+++ test/plugin/test_in_zmq_sub.rb
@@ -1,5 +1,6 @@
 require 'helper'
 require 'ffi-rzmq'
+require 'fluent/test/driver/input'
 
 class ZmqSubIutputTest < Test::Unit::TestCase
   def setup
@@ -8,7 +9,7 @@ class ZmqSubIutputTest < Test::Unit::TestCase
     @publisher = @context.socket(ZMQ::PUB)
     @publisher.bind("tcp://*:5556")
   end
-  
+
   def teardown
     @publisher.close
     @context.terminate
@@ -19,72 +20,67 @@ class ZmqSubIutputTest < Test::Unit::TestCase
      publisher #{PUBLISHER}
      subkey test1.,test2.
   ]
-  
-  
+
+
   def create_driver(conf=CONFIG)
-    Fluent::Test::InputTestDriver.new(Fluent::ZmqSubInput).configure(conf)
+    Fluent::Test::Driver::Input.new(Fluent::Plugin::ZmqSubInput).configure(conf)
   end
-  
+
   def test_configure
     d = create_driver(CONFIG + "bulk_send true")
     assert_equal PUBLISHER, d.instance.publisher
     assert_equal ["test1.","test2."], d.instance.subkeys
     assert_equal true, d.instance.bulk_send
   end
-  
+
   def test_receive
     d = create_driver
-    
+
     time = Time.parse("2011-01-02 13:14:15 UTC").to_i
     Fluent::Engine.now = time
-    
-    d.expect_emit "test1.aa", time, {"a"=>1}
-    d.expect_emit "test2.bb", time, {"a"=>2}
-    
-    d.run do
-      d.expected_emits.each {|tag,time,record|
+
+    expected_emits = [["test1.aa", time, {"a"=>1}],["test2.bb", time, {"a"=>2}]]
+
+    d.run(expect_emits: 1, timeout: 3) do
+      expected_emits.each {|tag,time,record|
         send_record("dummy",time,record)  # This record should not be received.
         send_record(tag,time,record)
       }
-      sleep 1
     end
+
+    assert_equal expected_emits, d.events
   end
 
   def test_no_subkey
     d = create_driver("")
-    
+
     time = Time.parse("2011-01-02 13:14:15 UTC").to_i
     Fluent::Engine.now = time
-    
-    d.expect_emit "test1.aa", time, {"a"=>1}
-    d.expect_emit "test2.bb", time, {"a"=>2}
-    
-    d.run do
-      d.expected_emits.each {|tag,time,record|
+
+    expected_emits = [["test1.aa", time, {"a"=>1}],["test2.bb", time, {"a"=>2}]]
+    d.run(expect_emits: 1, timeout: 3) do
+      expected_emits.each {|tag,time,record|
         send_record(tag,time,record)
       }
-      sleep 1
     end
+
+    assert_equal expected_emits, d.events
   end
 
 
   def test_receive_bulk
     d = create_driver(CONFIG + "bulk_send true")
-    
+
     time = Time.parse("2011-01-02 13:14:15 UTC").to_i
     Fluent::Engine.now = time
-    
-    d.expect_emit "test3.aa", time, {"a"=>1}
-    d.expect_emit "test4.bb", time, {"a"=>2}
-    
-    d.run do
-      record_to_send = []
-      d.expected_emits.each {|tag,time,record|
-        record_to_send << [tag,time,record]
-      }
+
+    record_to_send = [["test3.aa", time, {"a"=>1}],["test4.bb", time, {"a"=>2}]]
+
+    d.run(expect_emits: 1, timeout: 3) do
       send_record_bulk("test1.aa",record_to_send)
-      sleep 1
     end
+
+    assert_equal record_to_send, d.events
   end
 
   def send_record(tag,time,record)
